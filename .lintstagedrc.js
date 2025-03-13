0420f3fcdf0b9779b9c840649f13a4a7d98ff2b1
Shiv Bhonde <shivbhonde04@gmail.com>
2025-03-13 13:30:29 +0100

    lint warning in pre-commit hook

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 const buildNextEslintCommand = (filenames) =>
-  `yarn next:lint --fix --file ${filenames
+  `yarn next:lint --max-warnings=0 --fix --file ${filenames
     .map((f) => path.relative(path.join("packages", "nextjs"), f))
     .join(" --file ")}`;
 
