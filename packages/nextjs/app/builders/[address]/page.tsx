e5b1bf3c020fd7f6bd9015118299435a4918c953
Rinat <rinat@hey.com>
2025-04-04 18:08:12 +0300

    fix: refresh portfolio page (#89)

--- packages/nextjs/app/builders/[address]/page.tsx
+++ packages/nextjs/app/builders/[address]/page.tsx
@@ -1,6 +1,7 @@
 import { UpgradedToBGCard } from "./_components/UpgradedToBGCard";
 import { UserChallengesTable } from "./_components/UserChallengesTable";
 import { UserProfileCard } from "./_components/UserProfileCard";
+import { RouteRefresher } from "~~/components/RouteRefresher";
 import { isBgMember } from "~~/services/api-bg/builders";
 import { findLatestSubmissionPerChallengeByUser } from "~~/services/database/repositories/userChallenges";
 import { findUserByAddress } from "~~/services/database/repositories/users";
@@ -17,23 +18,26 @@ export default async function BuilderPage({ params }: { params: { address: strin
   }
 
   return (
-    <div className="max-w-7xl mx-auto px-4 py-8">
-      <div className="grid grid-cols-1 lg:grid-cols-4 gap-14">
-        <div className="lg:col-span-1">
-          <UserProfileCard user={user} address={userAddress} />
-        </div>
-        <div className="lg:col-span-3">
-          {bgMemberExists && <UpgradedToBGCard user={user} />}
-          <h2 className="text-2xl font-bold mb-0 text-neutral pb-4">Challenges</h2>
-          {challenges.length > 0 ? (
-            <UserChallengesTable challenges={challenges} />
-          ) : (
-            <div className="bg-base-100 p-8 text-center rounded-lg text-neutral">
-              This builder hasn&apos;t completed any challenges.
-            </div>
-          )}
+    <>
+      <RouteRefresher />
+      <div className="max-w-7xl mx-auto px-4 py-8">
+        <div className="grid grid-cols-1 lg:grid-cols-4 gap-14">
+          <div className="lg:col-span-1">
+            <UserProfileCard user={user} address={userAddress} />
+          </div>
+          <div className="lg:col-span-3">
+            {bgMemberExists && <UpgradedToBGCard user={user} />}
+            <h2 className="text-2xl font-bold mb-0 text-neutral pb-4">Challenges</h2>
+            {challenges.length > 0 ? (
+              <UserChallengesTable challenges={challenges} />
+            ) : (
+              <div className="bg-base-100 p-8 text-center rounded-lg text-neutral">
+                This builder hasn&apos;t completed any challenges.
+              </div>
+            )}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
