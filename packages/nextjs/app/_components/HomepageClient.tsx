89307fd449022f3e2e3ee072a7f6475a8f1d6c0d
abhi152003 <abhiprajapati011@gmail.com>
2025-05-06 16:50:37 +0530

    modified gitignore

--- packages/nextjs/app/_components/HomepageClient.tsx
+++ packages/nextjs/app/_components/HomepageClient.tsx
@@ -89,28 +89,6 @@ export const HomepageClient = ({ challenges }: { challenges: Challenges }) => {
           userChallenges={userChallenges}
           challenges={challenges}
         />
-
-        {/* <div className="flex flex-col xl:flex-row justify-center mx-auto relative">  
-          <AfterSreLine />  
-          <div className="hidden xl:flex flex-grow bg-[#96EAEA] dark:bg-[#3AACAD]" />  
-          <AfterSreCard  
-            title="ETH Tech Tree"  
-            description="Check this advanced Solidity challenges to test your Ethereum dev skills."  
-            externalLink="https://www.ethtechtree.com"  
-            buttonText="Join"  
-            previewImage="/assets/challenges/techTree.svg"  
-            bgClassName="bg-[#96EAEA] dark:bg-[#3AACAD]"  
-          />  
-          <AfterSreCard  
-            title="Capture the Flag"  
-            description="Join our CTF game and hack your way through 12 Smart Contract challenges."  
-            externalLink="https://ctf.buidlguidl.com"  
-            buttonText="Start"  
-            previewImage="/assets/challenges/ctf.svg"  
-            bgClassName="bg-base-300"  
-          />  
-          <div className="hidden xl:flex flex-grow bg-base-300" />  
-        </div>   */}
       </div>
     </div>
   );
