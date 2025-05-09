b8b31eb8f9de2f89a5de1d66fa6aed9c03201f44
abhi152003 <abhiprajapati011@gmail.com>
2025-05-09 14:16:37 +0530

    updated logos

--- packages/nextjs/app/_assets/icons/HeroLogo.tsx
+++ packages/nextjs/app/_assets/icons/HeroLogo.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import speedrunLogo from "../../../speedrun-logo.png";
+import speedrunLogo from "../../../speedrun-logo-new.png";
 
 const HeroLogo = ({ className = "" }: { className?: string }) => {
   return <Image src={speedrunLogo.src} alt="Speedrun Logo" className={className} width={300} height={100} />;
--- packages/nextjs/components/Logo.tsx
+++ packages/nextjs/components/Logo.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import speedrunLogo from "../../nextjs/speedrun-logo.png";
+import speedrunLogo from "../../nextjs/speedrun-logo-new.png";
 
 const Logo = ({ className = "" }: { className?: string }) => {
   return <Image src={speedrunLogo.src} alt="Speedrun Logo" className={className} width={300} height={100} />;
